3f809988ccd37f4c2c562d8acf2e40e90dfcc947
Ana Kokan <anaxkokan@gmail.com>
2021-04-06 14:58:55 +0100

    Improve style

--- sql_projects.sql
+++ sql_projects.sql
@@ -1,37 +1,35 @@
 --https://www.hackerrank.com/challenges/sql-projects/problem
 with project_start as (
     
-    select main.start_date
+    select main.start_date as project_start_date
     from projects as main 
     left join projects as previous on main.start_date = previous.end_date
-    where previous.start_date is NULL
+    where previous.start_date is null
     
 ),
 
 project_end as (
-    
-    
+        
     select main.end_date
     from projects as main 
     left join projects as next on next.start_date = main.end_date
-    where next.start_date is NULL
+    where next.start_date is null
 
 ),
 
 final as (
     
     select 
-    start_date,
-    min(end_date) as min_end_date
+    project_start_date,
+    min(end_date) as project_end_date
     from project_start
     full outer join project_end
-    on start_date < end_date
-    group by start_date
+    on project_start_date < end_date
+    group by project_start_date
     
 )
 
-select * 
-from final
+select * from final
 order by 
-    datediff(day, start_date, min_end_date),    
-    start_date;
+    datediff(day, project_start_date, project_end_date),    
+    project_start_date;
