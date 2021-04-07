bee253a45ad42885a966d20432ca292b91f8f328
Ana Kokan <anaxkokan@gmail.com>
2021-04-07 16:16:39 +0100

    Add solution to binary-search-tree-1

---
+++ binary_search_tree_1.sql
@@ -0,0 +1,10 @@
+--https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+select
+    n,
+    case 
+        when p is null then 'Root'
+        when n in (select p from bst) then 'Inner'
+        else 'Leaf'
+        end as node_type
+from bst
+order by n;
